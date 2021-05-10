70f8d284fc8b752b4554f5a3dbd8487f0e6ff82a
MRonald <ronaldmichael918@gmail.com>
2021-05-10 13:02:17 -0300

    Criando Header e estilizando | Alteração no nome do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+import Header from './components/Header';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        font-family: Ubuntu, sans-serif;
+    }
+`;
+
 export default function App() {
-  return (
-    <div className="App">
-      Meu app
-    </div>
-  );
+    return (
+        <div>
+            <GlobalStyle />
+            <Header />
+            <h1>Meu title</h1>
+        </div>
+    );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+    background-color: #2C94F5;
+    min-height: 60px;
+    width: 100vw;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    form {
+        width: 70%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        input {
+            padding: 10px;
+            width: 30%;
+            min-width: 195px;
+            border-radius: 10px;
+            border: 1px solid gray;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, .4);
+            font-family: Ubuntu;
+            font-size: 1rem;
+        }
+        input:not(:first-child) {
+            width: 12%;
+            margin: 0 15px;
+        }
+        input::placeholder {
+            color: black;
+        }
+        input:focus {
+            &::placeholder {
+                color: gray;
+            }
+        }
+        button {
+            padding: 10px;
+            font-size: 1rem;
+            color: white;
+            background-color: #1D6BB6;
+            border: 1px solid rgba(0, 0, 0, .4);
+            border-radius: 10px;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, .4);
+            cursor: pointer;
+        }
+    }
+`;
+
+export default function Header() {
+    return (
+        <HeaderWrapper>
+            <h1>PokeInfo</h1>
+            <form>
+                <input type="text" placeholder="Pesquisar por nome (Ex: Charmander)" />
+                <input type="text" placeholder="Pesquisar por categoria" />
+                <button>Buscar</button>
+            </form>
+        </HeaderWrapper>
+    );
+}
